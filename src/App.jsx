db3c164cb29dd50021ce12b87bbfb35b1b802e4a
Vasyl <vasya.chornous@gmail.com>
2025-04-15 02:24:27 +0300

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -52,6 +53,7 @@ const App = () => {
           element={<Orders />}
         />
       </Routes>
+      <Footer />
     </div>
   );
 };
